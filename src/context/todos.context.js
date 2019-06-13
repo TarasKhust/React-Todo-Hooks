96034ec8f7845b785e313391b15fbfac3db702bf
Taras Rushchak <taraskhust@gmail.com>
2019-06-13 21:03:15 +0300

    finish

--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,22 +1,26 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext } from 'react';
 import reducer from '../reducer/todo.reducer';
+import UseLocalStorageReducer from '../reducer/useLocalStorageReduce';
 
 const defaultTodos = [
-  { id: 1, task: 'Mow the lawn using goats', completed: false},
-  { id: 2, task: 'Release lady bugs into garden', completed: true}
+  { id: 1, task: 'Mow the lawn using goats', completed: false },
+  { id: 2, task: 'Release lady bugs into garden', completed: true }
 ];
 
 export const TodosContext = createContext();
-export const Dispatchcontext = createContext()
+export const Dispatchcontext = createContext();
 
 export function TodosProvider(props) {
-  const [todos, dispatch] = useReducer(reducer, defaultTodos)
+  const [todos, dispatch] = UseLocalStorageReducer(
+    'todos',
+    defaultTodos,
+    reducer
+  );
   return (
-    <TodosContext.Provider value={ todos }>
-      <Dispatchcontext.Provider value={ dispatch }>
-      {props.children}
+    <TodosContext.Provider value={todos}>
+      <Dispatchcontext.Provider value={dispatch}>
+        {props.children}
       </Dispatchcontext.Provider>
     </TodosContext.Provider>
-  )
+  );
 }
-
